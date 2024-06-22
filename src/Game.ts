f45b5e0efa9edb4015e7629ae4a116e63ef23469
lawrence415610 <34848993+lawrence415610@users.noreply.github.com>
2024-06-22 15:29:33 +1000

    The health and obstacles are on the grid now

--- src/Game.ts
+++ src/Game.ts
@@ -8,7 +8,8 @@ const blockLength = blockSample.sideLength;
 const maxBlockNumber =
     (canvasWidth / blockLength) * (canvasHeight / blockLength);
 export class Game {
-    private levelObstacleNumber = Math.floor(maxBlockNumber * 0.01) * this.level;
+    private levelObstacleNumber =
+        Math.floor(maxBlockNumber * 0.01) * this.level;
     private levelHealthNumber = Math.floor(maxBlockNumber * 0.01) * this.level;
     private obstacles: Block[] = [];
     private healths: Block[] = [];
@@ -16,17 +17,27 @@ export class Game {
         this.createHealth();
         this.createObstacle();
     }
-
-    getRandomNumber(x: number): number {
-        return Math.floor(Math.random() * (x + 1));
+    /**
+     * 
+     * @param x send the canvas width or height into this utility function
+     * @returns a x or y axis value that is on grid 
+     */
+    getValidAxisNumber(x: number): number {
+        const validAxisNumberList = [];
+        const maxBlockNumber = Math.floor(x / blockLength);
+        for (let i = 0; i < maxBlockNumber; i++) {
+            validAxisNumberList.push(i * blockLength);
+        }
+        const randomNumber = Math.floor(Math.random() * (maxBlockNumber + 1));
+        return validAxisNumberList[randomNumber];
     }
 
     createObstacle() {
         for (let i = 0; i < this.levelObstacleNumber; i++) {
             this.obstacles.push(
                 new Block(Category.OBSTACLE, {
-                    x: this.getRandomNumber(canvasWidth),
-                    y: this.getRandomNumber(canvasHeight),
+                    x: this.getValidAxisNumber(canvasWidth),
+                    y: this.getValidAxisNumber(canvasHeight),
                 })
             );
         }
@@ -36,8 +47,8 @@ export class Game {
         for (let i = 0; i < this.levelHealthNumber; i++) {
             this.healths.push(
                 new Block(Category.FOOD, {
-                    x: this.getRandomNumber(canvasWidth),
-                    y: this.getRandomNumber(canvasHeight),
+                    x: this.getValidAxisNumber(canvasWidth),
+                    y: this.getValidAxisNumber(canvasHeight),
                 })
             );
         }
