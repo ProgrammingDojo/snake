da51fb0c9c2a0e18070f1787bfe118cdc188a556
lawrence415610 <34848993+lawrence415610@users.noreply.github.com>
2024-06-27 23:01:35 +1000

    Organize Main, game can be set to close

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,86 @@
 import { Game } from "./Game.js";
 import { canvas } from "./Canvas.js";
+
+const canvasContext = canvas.canvasContext;
+
 class Main {
-    healthNumber = 0;
-    scoreNumber = 0;
+    private healthNumber:number;
+    private scoreNumber:number;
+    private intervalId: NodeJS.Timeout | null = null;
+    private isGameOver:boolean;
+
+    constructor(private game: Game) {
+        this.healthNumber = 3;
+        this.scoreNumber = 0;
+        this.isGameOver = false;
+    }
+
+    private decideGameOver(game: Game): void {
+        if (game.isGameover()) {
+            this.isGameOver = true;
+            this.stop();
+            this.cleanCanvas();
+            canvasContext.font = "bold 28px black serif";
+            canvasContext.fillText(
+                "GAME OVER",
+                canvas.canvasWidth / 2 - 87,
+                canvas.canvasHeight / 2 + 10
+            );
+            return;
+        }
+    }
+
+    private cleanCanvas(): void {
+        canvasContext.clearRect(0, 0, canvas.canvasWidth, canvas.canvasHeight);
+    }
 
     public main() {
-        const game = new Game();
-        const canvasContext = canvas.canvasContext;
-
-        setInterval(() => {
-            if (game.isGameover()) {
-                canvasContext.font = "bold 28px black serif";
-                canvasContext.fillText(
-                    "GAME OVER",
-                    canvas.canvasWidth / 2 - 100,
-                    canvas.canvasHeight / 2
-                );
-                return;
-            }
-            canvasContext.clearRect(
-                0,
-                0,
-                canvas.canvasWidth,
-                canvas.canvasHeight
-            );
-            game.init();
-            [this.healthNumber, this.scoreNumber] = game.setGameStatus();
-            const health =
-                (document.getElementsByClassName(
-                    "dashboard__health-number"
-                )[0] as HTMLElement) || null;
-            health.innerText = String(this.healthNumber);
-            const score =
-                (document.getElementsByClassName(
-                    "dashboard__score-number"
-                )[0] as HTMLElement) || null;
-            score.innerText = String(this.scoreNumber);
+        this.intervalId = setInterval(() => {
+            this.decideGameOver(this.game);
+            if(this.isGameOver) return;
+            this.cleanCanvas();
+            this.game.init();
+            [this.healthNumber, this.scoreNumber] = this.game.setGameStatus();
+            this.setHealthDashboard();
+            this.setScoreDashboard();
         }, 200);
+    }
+
+    public stop() {
+        if (this.intervalId !== null) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
+    }
+
+    private setHealthDashboard(): void {
+        const health =
+            (document.getElementsByClassName(
+                "dashboard__health-number"
+            )[0] as HTMLElement) || null;
+        health.innerText = String(this.healthNumber);
+    }
+
+    private setScoreDashboard(): void {
+        const score =
+            (document.getElementsByClassName(
+                "dashboard__score-number"
+            )[0] as HTMLElement) || null;
+        score.innerText = String(this.scoreNumber);
     }
 }
 
+let app: Main | null = null;
+
 function init() {
-    const app = new Main();
+    const game = new Game();
+    if (app) {
+        app.stop();
+    }
+    app = new Main(game);
     app.main();
 }
+
 init();
 
 const reset = (document.getElementById("reset") as HTMLButtonElement) || null;
